8b9c7fd627c9718284c3419cd3572588974029bd
Mohamedelbayoumi <mohamedalbayoumi822@gmail.com>
2026-01-24 19:59:45 +0200

    change sql query to get better results

--- controllers/live-location.controllers.js
+++ controllers/live-location.controllers.js
@@ -1,91 +1,64 @@
 const redisClient = require("../config/redis-client");
-const Booking = require("../models/Booking");
-const Driver = require("../models/Driver");
-const Guard = require("../models/Guard");
-const Guide = require("../models/Guide");
-const Translator = require("../models/Translator");
 const User = require("../models/User");
-const { Op } = require("sequelize");
+const { QueryTypes } = require("sequelize");
 const getDataFromRedis = require("../utils/get-data-from-redis");
 const emitter = require("../config/event-emitter");
+const sequelize = require("../config/db");
 
 const getLiveLocations = async (req, res) => {
 	const userId = req.userId;
 
-	console.log("ssssssssssssssssssssssssssss");
-
-	const bookings = await Booking.findAll({
-		where: {
-			user_id: userId,
-			[Op.or]: [
-				{ "$Guards.id$": { [Op.ne]: null } },
-				{ "$Drivers.id$": { [Op.ne]: null } },
-				{ "$Translators.id$": { [Op.ne]: null } },
-				{ "$Guides.id$": { [Op.ne]: null } },
-			],
-		},
-		subQuery: false,
-		attributes: [],
-		include: [
-			{
-				model: Guard,
-				required: false,
-				attributes: ["id"],
-				through: { attributes: [] },
-			},
-			{
-				model: Driver,
-				required: false,
-				attributes: ["id"],
-				through: { attributes: [] },
-			},
-			{
-				model: Translator,
-				required: false,
-				attributes: ["id"],
-				through: { attributes: [] },
-			},
-			{
-				model: Guide,
-				required: false,
-				attributes: ["id"],
-				through: { attributes: [] },
-			},
-		],
-	});
+	const bookings = await sequelize.query(
+		`SELECT protector_id, role FROM (
+    SELECT DB.driver_id AS protector_id, 'Driver' AS role FROM Bookings AS B LEFT JOIN Driver_Bookings AS DB ON DB.booking_id = B.id 
+WHERE B.user_id = ${userId}
+UNION ALL
+SELECT GB.guard_id AS protector_id, 'Guard' AS role FROM Bookings AS B LEFT JOIN Guard_Bookings AS GB ON GB.booking_id = B.id 
+WHERE B.user_id =${userId}
+UNION ALL
+SELECT GU.guide_id AS protector_id, 'Guide' AS role FROM Bookings AS B LEFT JOIN Guide_Bookings AS GU ON GU.booking_id = B.id 
+WHERE B.user_id = ${userId}
+UNION ALL
+SELECT TB.translator_id AS protector_id, 'Translator' AS role FROM Bookings AS B LEFT JOIN Translator_Bookings AS TB ON TB.booking_id = B.id 
+WHERE B.user_id = ${userId} ) AS User_Bookings
+WHERE protector_id IS NOT NULL;`,
+		{ type: QueryTypes.SELECT },
+	);
 
 	const guards = [];
 	const guides = [];
-	const trnaslators = [];
+	const translators = [];
 	const drivers = [];
 
 	for (const booking of bookings) {
-		if (booking.Guards) {
+		if (booking.role === "Guard") {
 			for (const guard of booking.Guards) {
-				guards.push(await getDataFromRedis("Guard", guard.id));
+				guards.push(await getDataFromRedis("Guard", guard.protector_id));
 			}
 		}
 
-		if (booking.Guides) {
+		if (booking.role === "Guide") {
 			for (const guide of booking.Guides) {
-				guides.push(await getDataFromRedis("Guide", guide.id));
+				guides.push(await getDataFromRedis("Guide", guide.protector_id));
 			}
 		}
 
-		if (booking.Translators) {
+		if (booking.role === "Translator") {
 			for (const translator of booking.Translators) {
-				trnaslators.push(await getDataFromRedis("Translator", translator.id));
+				translators.push(
+					await getDataFromRedis("Translator", translator.protector_id),
+				);
 			}
 		}
 
-		if (booking.Drivers) {
+		if (booking.role === "Driver") {
 			for (const driver of booking.Drivers) {
-				drivers.push(await getDataFromRedis("Driver", driver.id));
+				drivers.push(await getDataFromRedis("Driver", driver.protector_id));
 			}
 		}
 	}
 
-	res.status(200).json({ data: { guards, guides, trnaslators, drivers } });
+	res.status(200).json({ data: { guards, guides, translators, drivers } });
 };
 
 const saveLivelocation = async (req, res) => {
